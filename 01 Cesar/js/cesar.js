eda1511479f45aa04352db01e94b809115471eb3
GarciaGarciaAramJesua <agarciag1912@alumno.ipn.mx>
2021-10-04 00:54:42 -0500

    tmr se me olvido que iba primero el commit ;-;

---
+++ 01 Cesar/js/cesar.js
@@ -0,0 +1,53 @@
+var cesar = cesar || (function(){
+    var proceso = function(txt, desp, action){
+        var replace = (function() {
+            var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                        'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
+                        'x', 'y', 'z'];
+            var l = abc.length;
+            return function(c) {
+                var i = abc.indexOf(c.toLowerCase());
+                if(i != -1){
+                    var pos = i;
+                    if(action){
+                        pos += desp;
+                        pos -= (pos >= l)?l:0;
+                    }else{
+                        pos -= desp;
+                        pos += (pos < 0)?l:0;
+                    }
+                    return abc[pos];
+                }
+                return c;
+            };
+        })();
+        var re = (/([a-zñ])/ig);
+        return String(txt).replace(re, function(match){
+            return replace(match);
+        });
+    };
+
+    return{
+        encode : function(txt, desp){
+            return proceso(txt, desp, true);
+        },
+
+        decode : function(txt, desp){
+            return proceso(txt, desp, false);
+        }
+    };
+})();
+
+function cifrar(){
+    var matrizdesplaza = document.getElementById("desplaza").value;
+    matrizdesplaza %= 27;
+    document.getElementById("resultado").innerHTML =
+    cesar.encode(document.getElementById("cadena").value, matrizdesplaza);
+}
+
+function descifrar(){
+    var matrizdesplaza = document.getElementById("desplaza").value;
+    matrizdesplaza %= 27;
+    document.getElementById("resultado").innerHTML =
+    cesar.decode(document.getElementById("cadena").value, matrizdesplaza);
+}
